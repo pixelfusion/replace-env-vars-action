b8a64db5b3df6d5a06594921a2377f7321831180
Damian Mooyman <damian.mooyman@pixelfusion.co.nz>
2022-05-16 13:45:56 +1200

    fix: don't expose potential secret values

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ fi
 while IFS='=' read -r key value; do
   keyReplace=$(printf '__%s__\n' "$key" | sed -e 's/[]\/$*.^[]/\\&/g')
   valueReplace=$(printf '%s\n' "$value" | sed -e 's/[\/&]/\\&/g')
-  echo "Setting $key to $value "
+  echo "Setting $key"
   echo $value | wc -l
   sed -i "s/${keyReplace}/${valueReplace}/g" $FILENAME
 done < <(printenv)
